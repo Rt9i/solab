d5c0a372f3b3fbfeb69467fad34405a61d1d8692
Rt9i <158214881+Rt9i@users.noreply.github.com>
2024-12-10 12:49:21 +0200

    ..

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,5 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // Customize Webpack config here if needed
   return config;
 };
