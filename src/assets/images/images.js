627773d1b0c2f7f9e3dbb85ba97949753efab6c4
Rt9i <158214881+Rt9i@users.noreply.github.com>
2024-12-13 00:35:09 +0200

    .

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,7 +1,7 @@
 
 
 const Images = {
-  whiteLogo: () => require('../../../assets/images/whiteLogo.png'),
+  whiteLogo: () => require('./photos/whiteLogo.png'),
   dog: () => require('./photos/dog.png'),
   card: () => require('./photos/card.png'),
   pen: () => require('./photos/pen.png'),
