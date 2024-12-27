131e809dd0a6400a7ceabedd3e1b80445f68b3fd
Rt9i <158214881+Rt9i@users.noreply.github.com>
2024-12-27 02:29:29 +0200

    ..

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,13 @@
-const {getDefaultConfig} = require('expo/metro-config');
-const {mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
+const { mergeConfig } = require('@react-native/metro-config');
 
+// Set the Expo Router app root directory
 process.env.EXPO_ROUTER_APP_ROOT = 'app';
 
 const config = {
   resolver: {
     // Add any additional asset types you might need
-    assetExts: [
-      'db',
-      'mp4',
-      'jpg',
-      'png',
-      'ttf',
-      'wav',
-      'obj',
-      'fbx',
-      'svg',
-      'gif',
-      'json',
-    ],
+    assetExts: ['db', 'mp4', 'jpg', 'png', 'ttf', 'wav', 'obj', 'fbx', 'svg', 'gif', 'json'],
     // Add any additional source file types you might need
     sourceExts: ['js', 'jsx', 'ts', 'tsx', 'json', 'wasm', 'cjs'],
   },
