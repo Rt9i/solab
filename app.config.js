46fd0e3767af87b5bef0a0e874a19bb8080f24bf
Rt9i <158214881+Rt9i@users.noreply.github.com>
2024-12-27 04:05:16 +0200

    yea

---
+++ app.config.js
@@ -0,0 +1,14 @@
+export default {
+    expo: {
+      name: "solab",
+      slug: "solab",
+      platforms: ["ios", "android", "web"],
+      web: {
+        // Specify the base path if your app is hosted in a subfolder
+        router: {
+          basePath: "/solab", // Replace with your GitHub repository name
+        },
+      },
+    },
+  };
+  
